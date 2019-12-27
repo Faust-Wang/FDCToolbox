de38ab8b2249ec6b8bb8680e795d3da80bcf34a1
Parth Viradiya <36721553+parthp08@users.noreply.github.com>
2019-12-26 21:54:40 -0500

    Update __init__.py

--- FDCToolbox/__init__.py
+++ FDCToolbox/__init__.py
@@ -2,5 +2,5 @@ from .F16_Model import f16_model
 from .Trim import trim
 from .Linearization import linearize, get_lat_A, get_lat_B, get_lon_A, get_lon_B
 from .utils import print_array, print_matrix
-from RK4 import RK4
+from .RK4 import RK4
 
